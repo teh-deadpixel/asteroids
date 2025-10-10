cbbfd999eafa2d4706769fd2763efab4a04fe56b
teh-deadpixel <shroffharsh2@gmail.com>
2025-10-10 12:43:25 +0000

    Implemented groups for player update and draw, refactored game loop

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+    # print("TURN:", PLAYER_TURN_SPEED)
     while True:
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        player.update(dt)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
 
          
 if __name__ == "__main__":
